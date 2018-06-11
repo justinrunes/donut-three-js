bce1fbb3a55338046486289f3480eeef97bec3aa
justinrunes <justin@seveti.com>
2018-06-11 16:12:29 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# donut-three-js
+<h1 align="center">Donut Three.js</h1>
+<p align="center">
+    Donut using Three.js for Hari's competition</a>
+</p>
+
+## Quick start
+* Clone the repo: `git clone https://github.com/seveti/sassy-grid-starter.git`
+* Install Node.js dependencies: `npm install`
+* Run the app: `grunt` and open index.html
---
+++ src/js/script.js
@@ -0,0 +1,39 @@
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+var lights = [];
+lights[ 0 ] = new THREE.PointLight( 0xffffff, 1, 0 );
+lights[ 1 ] = new THREE.PointLight( 0xffffff, 1, 0 );
+lights[ 2 ] = new THREE.PointLight( 0xffffff, 1, 0 );
+
+lights[ 0 ].position.set( 0, 200, 0 );
+lights[ 1 ].position.set( 100, 200, 100 );
+lights[ 2 ].position.set( - 100, - 200, - 100 );
+
+scene.add( lights[ 0 ] );
+scene.add( lights[ 1 ] );
+scene.add( lights[ 2 ] );
+
+var geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
+var material = new THREE.MeshBasicMaterial( { color: 0xee9354 } );
+var torus = new THREE.Mesh( geometry, material );
+scene.add( torus );
+
+camera.position.z = 60;
+
+var animate = function () {
+    requestAnimationFrame( animate );
+
+    torus.rotation.x += 0.005;
+    torus.rotation.y += 0.005;
+
+    renderer.render( scene, camera );
+};
+
+animate();
